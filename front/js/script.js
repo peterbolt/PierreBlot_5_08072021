5b56cb5dfd75c5f3e1e4d10581217f618d859a4c
Pierre Blot <pierreremy.blot@gmail.com>
2021-12-02 10:49:54 +0100

    Affichage des produits sur la page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,40 @@
+let = kanapData = [];
+
+const fetchKanaps = async () => {
+  await fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => (kanapData = data))
+    .catch((error) => console.log(error));
+
+  console.log(kanapData);
+};
+
+const kanapDisplay = async () => {
+  await fetchKanaps();
+
+  kanapData.map((kanap) => {
+    const href = document.createElement("a");
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+
+    href.setAttribute("href", "product.html?id=" + kanap._id);
+    img.setAttribute("src", kanap.imageUrl);
+    img.setAttribute("alt", kanap.altTxt);
+    h3.className = "productName";
+    p.className = "productDescription";
+
+    h3.textContent = kanap.name;
+    p.textContent = kanap.description;
+
+    href.appendChild(article);
+    article.appendChild(img);
+    article.appendChild(h3);
+    article.appendChild(p);
+
+    document.getElementById("items").appendChild(href);
+  });
+};
+
+kanapDisplay();
